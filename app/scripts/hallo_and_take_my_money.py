183016cb6d723317061fc0d494e6f25ce015e209
shurikovyy <sanyochek95@gmail.com>
2025-07-05 12:41:04 +0300

    add dicript.

--- app/scripts/hallo_and_take_my_money.py
+++ app/scripts/hallo_and_take_my_money.py
@@ -1,5 +1,7 @@
+import logging
+
 def print_hallo():
-    print('Take my money, bitch!')
+    logging.info('every 2 min: Take my money, bitch!')
 
 if __name__=="__main__":
     print_hallo()
--- app/scripts/hallo_fucking_world.py
+++ app/scripts/hallo_fucking_world.py
@@ -1,7 +1,7 @@
 import logging
 
 def print_greeting():
-    logging.info('Shut up and smile!')
+    logging.info('every 1 min: Shut up and smile!')
 
 if __name__=="__main__":
     print_greeting()
