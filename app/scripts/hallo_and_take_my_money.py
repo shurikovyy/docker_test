3eadeecc35db0c274b586bec17cb782ebf037d34
shurikovyy <sanyochek95@gmail.com>
2025-07-05 12:50:41 +0300

    bla-bla

--- app/scripts/hallo_and_take_my_money.py
+++ app/scripts/hallo_and_take_my_money.py
@@ -1,7 +1,8 @@
 import logging
 
 def print_hallo():
-    logging.info('every 2 min: Take my money, bitch!')
+    logging.info('logging every 2 min: Take my money, bitch!')
+    print('print every 2 min: Take my money, bitch!')
 
 if __name__=="__main__":
     print_hallo()
--- app/scripts/hallo_fucking_world.py
+++ app/scripts/hallo_fucking_world.py
@@ -2,6 +2,7 @@ import logging
 
 def print_greeting():
     logging.info('every 1 min: Shut up and smile!')
+    print('print every 1 min: Shut up and smile!')
 
 if __name__=="__main__":
     print_greeting()
