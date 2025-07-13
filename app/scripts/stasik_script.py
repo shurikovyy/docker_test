8590f3701472bc6f8cd7799503ca8b750eedc8ba
shurikovyy <sanyochek95@gmail.com>
2025-07-13 17:27:17 +0300

    bdbd

--- app/scripts/stasik_script.py
+++ app/scripts/stasik_script.py
@@ -1,7 +1,17 @@
 import logging
+import sys
 
+# Настройка корневого логгера
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout),  # Вывод в stdout
+        # logging.FileHandler('/var/log/python.log')  # Дополнительно в файл
+    ]
+)
 def nu_ti_stas():
-    logging.warning('Stasik - pidarasik!')
+    logging.warning('** Stasik - pidarasik! **')
 
 if __name__=="__main__":
     nu_ti_stas()
