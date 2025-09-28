e4e3906f4756715cd54a8d52dc22d29396423250
shurikovyy <sanyochek95@gmail.com>
2025-09-28 16:22:29 +0300

    cron, requirements test

--- app/scripts/debug_script.py
+++ app/scripts/debug_script.py
@@ -3,5 +3,5 @@ print(">>> ENV DUMP:", {
     "POSTGRES_USER": os.getenv("POSTGRES_USER"),
     "POSTGRES_PASSWORD": os.getenv("POSTGRES_PASSWORD"),
     "POSTGRES_DB": os.getenv("POSTGRES_DB"),
-    "DATABASE_URL": os.getenv("DATABASE_URL")
+    # "DATABASE_URL": os.getenv("DATABASE_URL")
 })
